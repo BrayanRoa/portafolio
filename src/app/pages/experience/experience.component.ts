3ac5873bb9c9effa6cf3ac61395a0e4752a76553
brayanroa <brayanroa38@gmail.com>
2025-11-17 16:36:03 -0500

    new work experience added (selaski)

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -5,14 +5,54 @@ import { CardComponent, ICard } from '../../components/card/card.component';
   selector: 'app-experience',
   imports: [CardComponent],
   templateUrl: './experience.component.html',
-  styleUrl: './experience.component.css'
+  styleUrl: './experience.component.css',
 })
 export class ExperienceComponent {
-
   public experiences: ICard[] = [
     {
       name: 'Brayan Roa ✨',
-      date: 'March 2025 - Present',
+      date: 'August 2025 - Currently',
+      tag: ' 🏢 Selaski',
+      title: 'Full stack developer',
+      description: `
+        Company specialized in international trade software, where I contribute to the development, optimization, and maintenance of core platform functionalities.
+        Working with modern technologies on both frontend and backend has strengthened my ability to deliver robust, scalable, and user-focused solutions.
+        This role has allowed me to improve my problem-solving skills and collaborate closely with cross-functional teams to enhance the overall product experience.
+        
+        Key Contributions:
+
+        • Developed and maintained backend services using Node.js, TypeScript, Prisma ORM, and PostgreSQL, ensuring high performance and reliability.
+
+        • Built responsive and dynamic frontend modules using Angular 20, improving user experience and workflow efficiency for international trade operations.
+        
+        • Implemented new features across the entire stack—from requirement gathering to deployment—ensuring alignment with business and regulatory needs.
+        
+        • Collaborated with QA, product, and business teams to improve system stability, 
+        fix critical issues, and optimize existing functionalities.
+        
+        • Enhanced code quality through best practices, clean architecture principles, and continuous refactoring efforts.
+        `,
+      link: {
+        url: 'https://welcome.selaski.com',
+        label: 'Visit the web site',
+      },
+      hashtags: [
+        'Backend',
+        'Node.js',
+        'Nest js',
+        'Prisma ORM',
+        'MySql',
+        'Frontend',
+        'Angular',
+        'Prime NG',
+        'Tailwind',
+      ],
+      profileImg: 'profile.jpeg',
+      mainImg: 'selaski.png', // Puedes agregar una imagen principal
+    },
+    {
+      name: 'Brayan Roa ✨',
+      date: 'March 2025 - November 2025 (9 Months)',
       tag: ' 🏢 Broers',
       title: 'Backend Developer',
       description: `
@@ -33,15 +73,15 @@ export class ExperienceComponent {
       `,
       link: {
         url: 'https://www.linkedin.com/company/broerscol/posts/?feedView=all', // actualiza si tienes otra URL oficial
-        label: 'Visit the LinkedIn'
+        label: 'Visit the LinkedIn',
       },
       hashtags: ['Node.js', 'Python', 'Backend', 'ICFES', 'NestJS'],
       profileImg: 'profile.jpeg',
-      mainImg: 'broers.png' // puedes usar el logo o imagen que prefieras
+      mainImg: 'broers.png', // puedes usar el logo o imagen que prefieras
     },
     {
       name: 'Brayan Roa ✨',
-      date: 'April 2023 - November 2025',
+      date: 'April 2023 - November 2025 (20 Months)',
       tag: ' 🏢 Prisma Digital',
       title: 'Backend Developer',
       description: `
@@ -59,12 +99,11 @@ export class ExperienceComponent {
         `,
       link: {
         url: 'https://www.linkedin.com/company/prisma-dig/',
-        label: 'Visit the linkedin'
+        label: 'Visit the linkedin',
       },
       hashtags: ['Node.js', 'Python', 'Backend', 'Flask', 'Postgres'],
       profileImg: 'profile.jpeg',
-      mainImg: 'prisma-digital.png' // Puedes agregar una imagen principal
+      mainImg: 'prisma-digital.png', // Puedes agregar una imagen principal
     },
-
-  ]
+  ];
 }
